2d568d167d1a33126c506f601edc8fa650731290
smithb <smithb@wlwv.k12.or.us>
2022-11-05 23:55:16 -0700

    added extra case

--- src/Bill.java
+++ src/Bill.java
@@ -12,8 +12,9 @@ class Bill
 
     // Add a tip based on the number of guests
     //if the total number of customers is over 8, 25% extra
-    //if the total number of customers is over 4 but less than 8, 20% extra
-    //less than 4, 5% extra
+    //if the total number of customers is 4 or more but less than 8, 20% extra
+    //less than 4 but 2 or more, 5% extra
+    //less than 2, nothing extra
     public void addTip()
     {
 
