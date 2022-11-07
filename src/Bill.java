ac1093f3213b05de18ba9e62820658b9056e10f7
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2022-11-07 14:48:24 -0800

    clarification in 25% charge
    8 or more is 25% not just more than 8

--- src/Bill.java
+++ src/Bill.java
@@ -11,7 +11,7 @@ class Bill
     }
 
     // Add a tip based on the number of guests
-    //if the total number of customers is over 8, 25% extra
+    //if the total number of customers is 8 or more, 25% extra
     //if the total number of customers is 4 or more but less than 8, 20% extra
     //less than 4 but 2 or more, 5% extra
     //less than 2, nothing extra
