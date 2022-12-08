834686ca18f7be259be497f25a98c27e180c0f10
Brandon Smith <smithb@wlwv.k12.or.us>
2022-12-08 09:42:14 -0800

    examples done

--- CodeHSU04L02/src/MoreLoopFun.java
+++ CodeHSU04L02/src/MoreLoopFun.java
@@ -6,20 +6,32 @@ public class MoreLoopFun {
         }
          */
 
-        //a for loop that counts by ones from 1 to 5
-        for(int start = 1; start < 5; start++ ){
+        //a for loop that counts by ones from 1 to 6
+        for(int start = 1; start < 7; start++ ){
             System.out.println(start);
         }
 
-        //a for loop that prints the powers of two up to 100
+        //a for loop that prints the integer powers of two up to 100
         //can you have an infinite for loop?
-
-
+        System.out.println("Powers of 2");
+        for(int start = 1 ; start <= 100 ; start *= 2 ){
+            System.out.print(start + " ");
+        }
+        System.out.println();
+        System.out.println("Counting by 4s");
         //write a for loop that counts by 4s from 5 up to 50
+        for(int i = 5; i <= 50; i += 4){
+            System.out.print(i + " ");
+        }
 
-
+        System.out.println();
+        System.out.println("Summing 1 through 10");
         //write a for loop that sums the numbers from 1 to 10
         int total = 0;
+        for(int i = 1; i <= 10 ; i++){
+            total += i;
+        }
+        System.out.println("The sum of the integers 1 through 10 is: " + total);
 
         ////////////////////////////////////////////////////
         //for loops compared to while loops////////////////
@@ -37,5 +49,18 @@ public class MoreLoopFun {
         }
         System.out.println("Finished!");
 
+        System.out.println();
+        System.out.println("While loop");
+        String name = "Mr. Smith";
+        int j = 0;
+        while(j < name.length()){
+            System.out.println(name.charAt(j));
+            j++;
+        }
+        System.out.println();
+        System.out.println("For loop");
+        for(int i = 0; i<name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
     }
 }
